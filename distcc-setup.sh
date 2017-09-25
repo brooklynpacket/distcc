fd15063f59c52a247a52ef65a1b80072ec491527
Aaron Cantor <acantor@tinyco.com>
2017-09-25 14:31:35 -0700

    Updated scripts

--- distcc-setup.sh
+++ distcc-setup.sh
@@ -2,19 +2,47 @@
 
 set -e
 
-brew install autoconf
-brew install automake
-brew install distcc
+setup()
+{
+	brew install autoconf
+	brew install automake
+	brew install distcc
 
-sudo git clone https://github.com/PSPDFKit-labs/distcc /usr/local/tinyco/distcc
-cd /usr/local/tinyco/distcc
-sudo git clone https://github.com/brooklynpacket/distcc tinyco
+	sudo git clone https://github.com/PSPDFKit-labs/distcc /usr/local/tinyco/distcc
+	cd /usr/local/tinyco/distcc
+	sudo git clone https://github.com/brooklynpacket/distcc tinyco
 
-sudo ./autogen.sh
-sudo ./configure --without-libiberty --disable-Werror
-sudo make
+	sudo ./autogen.sh
+	sudo ./configure --without-libiberty --disable-Werror
+	sudo make
 
-sudo cp distcc distccd distccmon-text lsdistcc pump /usr/local/Cellar/distcc/3.2rc1/bin/.
-sudo cp tinyco/org.distccd.user.plist /Library/LaunchDaemons/.
-sudo chown root:wheel /Library/LaunchDaemons/org.distccd.user.plist
-sudo chmod 644 /Library/LaunchDaemons/org.distccd.user.plist
+	sudo cp distcc distccd distccmon-text lsdistcc pump /usr/local/Cellar/distcc/3.2rc1/bin/.
+	sudo cp tinyco/org.distccd.user.plist /Library/LaunchDaemons/.
+	sudo chown root:wheel /Library/LaunchDaemons/org.distccd.user.plist
+	sudo chmod 644 /Library/LaunchDaemons/org.distccd.user.plist
+
+	tinyco/distccd.sh
+}
+
+usage()
+{
+	echo "Options:\n\n"
+	echo "-i                                 Install distcc, set up a launch daemon, and start build server"
+	echo "-u                                 Uninstall distcc and unregister as a build server"
+	echo "-p /Path/To/project.xcodeproj      Set up XCode project to build using distcc"
+	echo "-c /Path/To/project.xcodeproj      Remove distcc from project build settings"
+}
+
+while [ "$1" != "" ]
+do
+	case $1 in
+		-i )	setup
+				;;
+		-h )	usage
+				exit
+				;;
+		* )		usage
+				exit 1
+	esac
+	shift
+done
--- distccd.sh
+++ distccd.sh
@@ -4,14 +4,15 @@ set -e
 
 cd /usr/local/tinyco/distcc/tinyco
 
-git fetch origin
-git merge origin/master
+sudo git fetch origin
+sudo git merge origin/master
 
 CL_VERSION=$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables | grep version:)
 echo "CLI Version: $CL_VERSION"
 
-REQUIRED_VERSION=$(cat hosts | grep version:)
-if [ "$CL_VERSION" != "$REQUIRED_VERSION" ]; then
+REQUIRED_VERSION=$(sudo cat hosts | grep version:)
+if [ "$CL_VERSION" != "$REQUIRED_VERSION" ]
+then
 	echo "Local command line tools $CL_VERSION does not match reßquired $REQUIRED_VERSION"
 	exit 1
 fi
@@ -19,15 +20,16 @@ fi
 MAC_ADDRESS=$(ifconfig en0 | grep ether | sed 's/.*ether \(.*\)/\1/' | sed 's/0\([0-9A-Fa-f]\)/\1/g' | tr '[:upper:]' '[:lower:]')
 echo "MAC Address: $MAC_ADDRESS"
 
-HOSTS_MAC=$(cat ./hosts | grep $MAC_ADDRESS)
+HOSTS_MAC=$(sudo cat ./hosts | grep $MAC_ADDRESS)
 echo "Hosts MAC: $HOSTS_MAC"
 
-if [ "$HOSTS_MAC" == "" ]; then
+if [ "$HOSTS_MAC" == "" ]
+then
 	echo "Adding address to hosts file"
 	echo "\n$MAC_ADDRESS" >> ./hosts
-	git add hosts
-	git commit -m "distccd.sh added MAC address to host file"
-	git push origin master
+	sudo git add hosts
+	sudo git commit -m "distccd.sh added MAC address to host file"
+	sudo git push origin master
 fi
 
-/usr/local/bin/distccd --no-detach --daemon --allow 127.0.0.1 --log-stderr
+/usr/local/bin/distccd --no-detach --daemon --allow 127.0.0.1 --log-stderr --verbose
