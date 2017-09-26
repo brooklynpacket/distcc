a095068593df3dcd25da9554f693298dcef7b910
Aaron Cantor <acantor@tinyco.com>
2017-09-26 16:11:24 -0700

    Back to using MAC addresses

--- distcc-precompile.sh
+++ distcc-precompile.sh
@@ -5,4 +5,16 @@ set -e
 cd ~/Documents/tinyco/distcc
 git fetch origin
 git merge origin/master
-cp hosts ~/.distcc/hosts
+
+ping 10.1.3.255 -W 2000
+echo "127.0.0.1" > "hosts.temp"
+
+while read line || [[ -n "$line" ]]
+do
+	if [ "$line" != "" ]
+	then
+		arp -na | grep "$line" | sed 's/.*(\(.*\)).*/\1/' >> "hosts.temp"
+	fi
+done < "hosts"
+
+mv hosts.temp ~/.distcc/hosts
--- distccd.sh
+++ distccd.sh
@@ -18,24 +18,28 @@ sudo git merge origin/master
 CL_VERSION=$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables | grep version:)
 echo "CLI Version: $CL_VERSION"
 
+MAC_ADDRESS=$(ifconfig en0 | grep ether | sed 's/.*ether \(.*\)/\1/' | sed 's/0\([0-9A-Fa-f]\)/\1/g' | tr '[:upper:]' '[:lower:]')
+echo "MAC Address: $MAC_ADDRESS"
+
 REQUIRED_VERSION=$(cat cli-version | grep version:)
 if [ "$CL_VERSION" != "$REQUIRED_VERSION" ]
 then
 	echo "Local command line tools $CL_VERSION does not match required $REQUIRED_VERSION"
-	echo "Removing network name from hosts file"
-	cat hosts | grep -v "$HOSTNAME" | sudo tee hosts
-	COMMIT_MSG="distccd.sh removed network name from host file"
+	echo "Removing MAC address from hosts file"
+	cat hosts | grep -v "$MAC_ADDRESS" | sudo tee hosts
+	COMMIT_MSG="distccd.sh removed MAC address from host file"
 	commit_hosts
 	exit 1
 fi
 
-HOSTS_ID=$(cat hosts | grep "$HOSTNAME") || true
+HOSTS_ID=$(cat hosts | grep "$MAC_ADDRESS") || true
+echo "Hosts MAC: $HOSTS_ID"
 
 if [ "$HOSTS_ID" == "" ]
 then
-	echo "Adding name to hosts file"
-	echo "$HOSTNAME.\n" | sudo tee -a hosts
-	COMMIT_MSG="distccd.sh added network name to host file"
+	echo "Adding MAC address to hosts file"
+	echo "$MAC_ADDRESS" | sudo tee -a hosts
+	COMMIT_MSG="distccd.sh added MAC address to host file"
 	commit_hosts
 fi
 
