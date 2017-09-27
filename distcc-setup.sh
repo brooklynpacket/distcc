1dea38e3f107480810a22045dccfa992fcf1ca15
Aaron Cantor <acantor@tinyco.com>
2017-09-27 11:56:49 -0700

    Server now launches as agent rather than daemon, and only on wired connection

--- distcc-setup.sh
+++ distcc-setup.sh
@@ -9,30 +9,22 @@ setup()
 	brew install automake
 	brew install distcc
 
-	sudo git clone https://github.com/PSPDFKit-labs/distcc /usr/local/tinyco/distcc
-	cd /usr/local/tinyco/distcc
-	sudo git clone https://github.com/brooklynpacket/distcc tinyco
+	git clone https://github.com/PSPDFKit-labs/distcc ~/distcc
+	cd ~/distcc
+	git clone https://github.com/brooklynpacket/distcc tinyco
 
-	sudo ./autogen.sh
-	sudo ./configure --without-libiberty --disable-Werror
-	sudo make
+	./autogen.sh
+	./configure --without-libiberty --disable-Werror
+	make
 
 	sudo cp distcc distccd distccmon-text lsdistcc pump /usr/local/Cellar/distcc/3.2rc1/bin/.
-	sudo cp tinyco/org.distccd.user.plist /Library/LaunchDaemons/.
-	sudo chown root:wheel /Library/LaunchDaemons/org.distccd.user.plist
-	sudo chmod 644 /Library/LaunchDaemons/org.distccd.user.plist
+	cp tinyco/org.distccd.user.plist ~/Library/LaunchAgents/.
+	sudo chown root:wheel ~/Library/LaunchAgents/org.distccd.user.plist
+	sudo chmod 666 ~/Library/LaunchAgents/org.distccd.user.plist
 
 	/usr/local/Cellar/distcc/3.2rc1/bin/distcc || true
 
 	tinyco/distccd.sh
-}
-
-setup_project()
-{
-	if [ ! -d "~/Documents/tinyco/distcc" ]
-	then
-		git clone https://github.com/brooklynpacket/distcc ~/Documents/tinyco/distcc
-	fi
 
 	sudo touch ~/.distcc/hosts
 	sudo chmod 666 ~/.distcc/hosts
@@ -40,21 +32,14 @@ setup_project()
 
 usage()
 {
-	echo "Options:\n\n"
-	echo "-i                                 Install distcc, set up a launch daemon, and start build server"
-	echo "-u                                 Uninstall distcc and unregister as a build server"
-	echo "-p /Path/To/project.xcodeproj      Set up XCode project to build using distcc"
-	echo "-c /Path/To/project.xcodeproj      Remove distcc from project build settings"
+	echo "Options:\n"
+	echo "-i       Install distcc, set up a launch agent, and start build server"
 }
 
 while [ "$1" != "" ]
 do
 	case $1 in
 		-i )	setup
-				exit
-				;;
-		-p )	project_path=$1
-				setup_project
 				exit
 				;;
 		-h )	usage
